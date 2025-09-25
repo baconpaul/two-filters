503a9aa7662c8efeb5a30eb5fdd0113c1075f6ae
baconpaul <paul@pwjw.com>
2025-09-25 12:39:04 -0400

    Try to get it working - windows not there

--- cmake/basic_installer_clapfirst.cmake
+++ cmake/basic_installer_clapfirst.cmake
@@ -54,33 +54,30 @@ function(add_clapfirst_installer)
                 COMMAND echo ${CMAKE_SOURCE_DIR}/libs/sst/sst-plugininfra/scripts/installer_mac/make_installer.sh ${CIN_PRODUCT_NAME} ${CIN_ASSET_OUTPUT_DIR} ${CMAKE_SOURCE_DIR}/resources/installer_mac ${CMAKE_BINARY_DIR}/installer "${INST_DATE}-${GIT_COMMIT_HASH}"
                 COMMAND ${CMAKE_SOURCE_DIR}/libs/sst/sst-plugininfra/scripts/installer_mac/make_installer.sh ${CIN_PRODUCT_NAME} ${CIN_ASSET_OUTPUT_DIRECTORY} ${CMAKE_SOURCE_DIR}/resources/installer_mac ${CMAKE_BINARY_DIR}/installer "${INST_DATE}-${GIT_COMMIT_HASH}"
         )
-    endif()
-
-    if (FALSE)
-    #elseif (WIN32)
-    if (WIN32)
+    elseif (WIN32)
         message(STATUS "Configuring for win installer")
         include(InnoSetup)
         install_inno_setup()
-        cmake_path(REMOVE_EXTENSION OBXF_ZIP OUTPUT_VARIABLE OBXF_INSTALLER)
+        cmake_path(REMOVE_EXTENSION INST_ZIP OUTPUT_VARIABLE WIN_INSTALLER)
         add_custom_command(
-            TARGET obxf-installer
+            TARGET ${TGT}
             POST_BUILD
+            USES_TERMINAL
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMAND ${CMAKE_COMMAND} -E make_directory installer
-            COMMAND 7z a -r installer/${OBXF_ZIP} ${OBXF_PRODUCT_DIR}/
-            COMMAND ${CMAKE_COMMAND} -E echo "ZIP Installer in: installer/${OBXF_ZIP}"
+            COMMAND 7z a -r installer/${INST_ZIP} ${ASSET_OUTPUT_DIRECTORY}/
+            COMMAND ${CMAKE_COMMAND} -E echo "ZIP Installer in: installer/${INST_ZIP}"
             COMMAND ${INNOSETUP_COMPILER_EXECUTABLE}
-                /O"${CMAKE_BINARY_DIR}/installer" /F"${OBXF_INSTALLER}" /DName="${TARGET_BASE}"
-                /DNameCondensed="${TARGET_BASE}" /DVersion="${GIT_COMMIT_HASH}"
-                /DID="BBE27B03-BDB9-400E-8AC1-F197B964651A"
+                /O"${CMAKE_BINARY_DIR}/installer" /F"${WIN_INSTALLER}" /DName="${PRODUCT_NAME}"
+                /DNameCondensed="${PRODUCT_NAME}" /DVersion="${GIT_COMMIT_HASH}"
+                /DID="a74e3385-ee81-404d-b2ce-93452c512018"
                 /DCLAP /DVST3 /DSA
                 /DIcon="${CMAKE_SOURCE_DIR}/resources/installer/logo.ico"
                 /DBanner="${CMAKE_SOURCE_DIR}/resources/installer/banner.png"
                 /DArch="${INNOSETUP_ARCH_ID}"
                 /DLicense="${CMAKE_SOURCE_DIR}/LICENSE"
-                /DStagedAssets="${OBXF_PRODUCT_DIR}"
-                /DData="${CMAKE_SOURCE_DIR}/assets/installer" "${INNOSETUP_INSTALL_SCRIPT}"
+                /DStagedAssets="${ASSETS_OUTPUT_DIRECTORY}"
+                "${INNOSETUP_INSTALL_SCRIPT}"
         )
     else ()
         message(STATUS "Basic Installer: Target is installer/${OBXF_ZIP}")
@@ -89,24 +86,16 @@ function(add_clapfirst_installer)
                 POST_BUILD
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 COMMAND ${CMAKE_COMMAND} -E make_directory installer
-                COMMAND ${CMAKE_COMMAND} -E tar cvf installer/${OBXF_ZIP} --format=zip ${OBXF_PRODUCT_DIR}/
-                COMMAND ${CMAKE_COMMAND} -E echo "Installer in: installer/${OBXF_ZIP}")
+                COMMAND ${CMAKE_COMMAND} -E tar cvf installer/${INST_ZIP} --format=zip ${ASSET_OUTPUT_DIRECTORY}/
+                COMMAND ${CMAKE_COMMAND} -E echo "Installer in: installer/${INST_ZIP}")
 
-        add_custom_command(
-                TARGET obxf-installer
-                POST_BUILD
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-                USES_TERMINAL
-                COMMAND scripts/installer_linux/make_deb.sh ${OBXF_PRODUCT_DIR} ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR}/installer "${OBXF_DATE}-${GIT_COMMIT_HASH}"
-        )
-        # Only build the assets zip on linux, to be CI friendly
-        add_custom_command(
-                TARGET obxf-installer
-                POST_BUILD
-                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/assets/installer
-                COMMAND ${CMAKE_COMMAND} -E tar cvf ${CMAKE_BINARY_DIR}/installer/${OBXF_ASSETS_ZIP} --format=zip .
-                COMMAND ${CMAKE_COMMAND} -E echo "Installer assets: installer/${OBXF_ASSETS_ZIP}")
+        #add_custom_command(
+        #        TARGET obxf-installer
+        #        POST_BUILD
+        #        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        #        USES_TERMINAL
+        #        COMMAND scripts/installer_linux/make_deb.sh ${OBXF_PRODUCT_DIR} ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR}/installer "${OBXF_DATE}-${GIT_COMMIT_HASH}"
+        #)
 
     endif ()
-        endif()
 endfunction()
